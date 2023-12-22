1df360e44bf492565dac31725d745e1d5e038cab
PaudelSworup <sworupkc9@gmail.com>
2023-12-22 21:58:14 +0545

    railway production

--- config.tsx
+++ config.tsx
@@ -1,2 +1,4 @@
-export const API: string = "https://netflix-f5xu.onrender.com/api";
+// export const API: string = "https://netflix-f5xu.onrender.com/api";
+export const API: string =
+  "https://netflixbackend-production-c41a.up.railway.app/api";
 export const imageURL: string = "https://image.tmdb.org/t/p/original";
